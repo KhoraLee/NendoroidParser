2b992b34ad3f221636ee74dfc3e5beb5d25658a4
KhoraLee <khora.lee.0@gmail.com>
2022-05-31 02:26:43 +0900

    feat: Implement multithreading by parallelStream

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,7 @@
 import Data.Nendoroid
 import java.io.File
+import java.util.*
+import java.util.concurrent.atomic.AtomicInteger
 
 fun main() {
     val parser = Parser()
@@ -10,48 +12,50 @@ fun main() {
     parser.gsc.parseByYear() // 년도별 페이지 파싱
     parser.csv.parseNendoroid("sheet.csv") // 넨겔 시트 DB 부분 추출
     updateNendoroids() // 공홈 각 넨도별 페이지 파싱
-    checkAllDB()
+//    checkAllDB()
 }
 
-fun updateNendoroids(path: String = "") {
+fun updateNendoroids() {
     val parser = Parser()
-
-    val path = File(path.ifEmpty { "D:\\NendoroidDB\\Nendoroid" })
-    val fList: Array<File> = path.listFiles()
-
-    fList.forEach {
-        if (it.isDirectory) {
-            updateNendoroids(it.path)
-            return@forEach
-        }
+    val nendoroidList = getAllNendoroids()
+    val total = nendoroidList.size
+    val current = AtomicInteger()
+    nendoroidList.parallelStream().forEach {
+        current.incrementAndGet()
         if (it.nameWithoutExtension == "1538") return@forEach // 특설페이지로 파싱 불가
         val diskNendoroid = Nendoroid.readNendoroid(it.nameWithoutExtension) ?: return@forEach
         val parsedNendoroid = parser.gsc.parseNendoroid(diskNendoroid)
         diskNendoroid.merge(parsedNendoroid)
         diskNendoroid.writeOnDisk()
-        print("${it.nameWithoutExtension}\t")
+        println("[${String.format("%04d", current.get())} / $total] ${diskNendoroid.num}\t(Thread : " + Thread.currentThread().name + ")")
+        Thread.sleep(50)
     }
-    println()
+}
+fun checkAllDB() {
+    val nendoroidList = getAllNendoroids()
+    val list = Collections.synchronizedList(ArrayList<String>())
+    nendoroidList.parallelStream().forEach { it ->
+        val dn = Nendoroid.readNendoroid(it.nameWithoutExtension) ?: return@forEach
+        if (dn.price == -1 || dn.price == 0) {
+            list.add(dn.num)
+        }
+    }
+    list.sorted().forEach {
+        println(it)
+    }
 }
 
-fun checkAllDB(path: String = "") {
-    val sb = StringBuilder()
+private fun getAllNendoroids(path: String = ""): ArrayList<File> {
+    val nendoroidList = ArrayList<File>()
     val path = File(path.ifEmpty { "D:\\NendoroidDB\\Nendoroid" })
-    val fList: Array<File> = path.listFiles()
-
-    sb.append("번호,이름,시리즈,이름_KO,시리즈_KO\n")
-
-    fList.forEach { it ->
-        if (it.isDirectory) {
-            sb.append(checkAllDB(it.path))
+    val fList = path.listFiles()
+    fList.forEach {
+        if(it.isDirectory) {
+            nendoroidList.addAll(getAllNendoroids(it.path))
             return@forEach
         }
-        val dn = Nendoroid.readNendoroid(it.nameWithoutExtension) ?: return@forEach
-        dn.release_date.forEach { str ->
-            if (str.length != 7){
-                println(str)
-            }
-        }
+        nendoroidList.add(it)
     }
-
+    return nendoroidList
 }
+
