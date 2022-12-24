0414b4b596f2131d42c4d7841de889e170fbf5cf
Seungyun Lee <khora.lee.0@gmail.com>
2022-12-24 15:11:09 +0900

    chore: Add linting and formatting process
    SwiftLint has problem with SPM versioning so specified latest release's commit revision for workaround.

---
+++ BuildTools/Package.swift
@@ -0,0 +1,11 @@
+// swift-tools-version:5.6
+import PackageDescription
+
+let package = Package(
+  name: "BuildTools",
+  platforms: [.macOS(.v10_11)],
+  dependencies: [
+    .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.49.0"),
+    .package(url: "https://github.com/realm/SwiftLint", revision: "a876e860ee0e166a05428f430888de5d798c0f8d"),
+  ],
+  targets: [.target(name: "BuildTools", path: "")])
