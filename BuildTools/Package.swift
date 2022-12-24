22674974d4491ec51961077976f230bdcda89cd3
Seungyun Lee <khora.lee.0@gmail.com>
2022-12-24 16:13:00 +0900

    chore: Update linter and formatter configuration

--- BuildTools/Package.swift
+++ BuildTools/Package.swift
@@ -5,7 +5,7 @@ let package = Package(
   name: "BuildTools",
   platforms: [.macOS(.v10_11)],
   dependencies: [
-    .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.49.0"),
+    .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.50.6"),
     .package(url: "https://github.com/realm/SwiftLint", revision: "a876e860ee0e166a05428f430888de5d798c0f8d"),
   ],
   targets: [.target(name: "BuildTools", path: "")])
